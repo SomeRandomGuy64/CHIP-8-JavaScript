17aed92a6f75413006fb3e5b66599e56f05b1ceb
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-05 18:48:24 +0000

    Constructor done

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -7,6 +7,22 @@ class Renderer
         //display size: 64x32
         this.cols = 64;
         this.rows = 32;
+
+
+        //The following code is what lets us scale the pixels up as a 64x32 resolution is incredibly small on a modern display
+        this.scale = scale;
+
+        this.canvas = document.querySelector('canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+        //canvas width = column size which is 64
+        this.canvas.width = this.cols * this.scale;
+        //canvas length = row size which is 32
+        this.canvas.height = this.rows * this.scale;
+
+        //an array for the display which will be a size of 64*32 or 2048
+        //Each item in the array will represent a pixel and will be either on or off as CHIP-8 is black and white
+        this.display = new Array(this.cols * this.rows)
     }
 }
 
