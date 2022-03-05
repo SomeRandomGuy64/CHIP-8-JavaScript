631f97432a21a9caa49a36b4593810777c15896d
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-05 20:09:29 +0000

    testRender()

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -93,6 +93,13 @@ class Renderer
             }
         }
     }
+
+    //testing purposes
+    testRender()
+    {
+        this.setPixel(0, 0);
+        this.setPixel(5, 2);
+    }
 }
 
 export default Renderer;
