f669bceb54e6906235552373c4e7927787e428f5
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-05 18:15:42 +0000

    Done a bit of the constructor

---
+++ scripts/renderer.js
@@ -0,0 +1,13 @@
+class Renderer 
+{
+
+    //constructor, takes an argument "scale" which allows me to scale the display
+    constructor(scale)
+    {
+        //display size: 64x32
+        this.cols = 64;
+        this.rows = 32;
+    }
+}
+
+export default Renderer;
