4291502ca4fc82b6cd06b752dcd314d6affa03e9
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-07 23:02:39 +0000

    speaker.js constructor

---
+++ scripts/speaker.js
@@ -0,0 +1,16 @@
+class Speaker
+{
+    contructor()
+    {
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+        this.audioCtx = new AudioContext();
+
+        //create a gain, which will allow us to control the volume
+        this.gain = this.audioCtx.createGain();
+        this.finish = this.audioCtx.destination;
+
+        //connect the gain to the audio context
+        this.gain.connect(this.finish);
+    }
+}
