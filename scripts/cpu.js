515c674f6f558d6a7e043337e8d108f2e3ae220a
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-08 23:57:57 +0000

    empty switch statement for opcodes

--- scripts/cpu.js
+++ scripts/cpu.js
@@ -162,6 +162,102 @@ class CPU
         //we only need the 3rd nibble, so grab the value of the 3rd nibble
         //and shift it right 4 bits to get rid of everything but that 3rd nibble
         let y = (opcode & 0xF00) >> 4;
+
+
+        
+        switch (opcode & 0xF000) {
+            case 0x0000:
+                switch (opcode) {
+                    case 0x00E0:
+                        break;
+                    case 0x00EE:
+                        break;
+                }
+        
+                break;
+            case 0x1000:
+                break;
+            case 0x2000:
+                break;
+            case 0x3000:
+                break;
+            case 0x4000:
+                break;
+            case 0x5000:
+                break;
+            case 0x6000:
+                break;
+            case 0x7000:
+                break;
+            case 0x8000:
+                switch (opcode & 0xF) {
+                    case 0x0:
+                        break;
+                    case 0x1:
+                        break;
+                    case 0x2:
+                        break;
+                    case 0x3:
+                        break;
+                    case 0x4:
+                        break;
+                    case 0x5:
+                        break;
+                    case 0x6:
+                        break;
+                    case 0x7:
+                        break;
+                    case 0xE:
+                        break;
+                }
+        
+                break;
+            case 0x9000:
+                break;
+            case 0xA000:
+                break;
+            case 0xB000:
+                break;
+            case 0xC000:
+                break;
+            case 0xD000:
+                break;
+            case 0xE000:
+                switch (opcode & 0xFF) {
+                    case 0x9E:
+                        break;
+                    case 0xA1:
+                        break;
+                }
+        
+                break;
+            case 0xF000:
+                switch (opcode & 0xFF) {
+                    case 0x07:
+                        break;
+                    case 0x0A:
+                        break;
+                    case 0x15:
+                        break;
+                    case 0x18:
+                        break;
+                    case 0x1E:
+                        break;
+                    case 0x29:
+                        break;
+                    case 0x33:
+                        break;
+                    case 0x55:
+                        break;
+                    case 0x65:
+                        break;
+                }
+        
+                break;
+        
+            default:
+                throw new Error('Unknown opcode ' + opcode);
+        }
     }
 }
 
