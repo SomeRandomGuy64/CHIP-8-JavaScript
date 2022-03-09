a16b15908d9c88aeedf8605a05bb20795ec5076f
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-09 00:28:35 +0000

    chip8.js Correction

--- scripts/chip8.js
+++ scripts/chip8.js
@@ -24,7 +24,7 @@ function init()
     //renderer.render();
     //end test
 
-    cpu.loadProgramIntoMemory();
+    cpu.loadSpritesIntoMemory();
     cpu.loadROM('BLITZ');
     loop = requestAnimationFrame(step);
 }
